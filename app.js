b56da9e515a2512f79e8400b65a35e4e4eb9f4bb
JOTHIRUBAN <jothiruban1512@gmail.com>
2023-12-03 11:58:14 +0530

    session enable

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require('express');
-//const passport = require('passport');
-//const LocalStrategy = require('passport-local').Strategy;
-//const session = require('express-session');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+const flash = require('express-flash');
 const ejs = require('ejs');
-//const bcrypt = require('bcrypt');
-//const { Pool } = require('pg');
-
 const app = express();
 
 // Set up session
-//app.use(session({
-//  secret: 'your-secret-key',
-//  resave: false,
-//  saveUninitialized: true
-//}));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
 
-// Initialize Passport
-//app.use(passport.initialize());
-//app.use(passport.session());
 
 // Set up body parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,6 +22,17 @@ app.use(bodyParser.json());
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
+
+//postgresql configuration
+const pool = new Pool({
+    user: 'jothiruban',
+    host: '10.13.26.67',
+    database: 'fitfuel',
+    password: 'postgres',
+    port: 5432,
+  });
+
+ 
 
 app.get("/",(req,res)=>{
     res.render("home");
@@ -37,6 +44,64 @@ app.get("/login",(req,res)=>{
 app.get("/signin",(req,res)=>{
     res.render("signin");
 })
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the user exists
+    const result = await pool.query('SELECT * FROM "user" WHERE email = $1', [email]);
+    const user = result.rows[0];
+
+    if (user) {
+      
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (passwordMatch) {
+   
+        console.log(user);
+        req.session.userId = user.user_id;
+        res.redirect('home');
+      
+      } else {
+     
+        return res.status(401).json({ message: 'Incorrect password' });
+      }
+    } else {
+      // User not found
+      return res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/home', (req, res) => {
+      res.send(`Welcome, ${req.session.userId}!`);
+    
+  });
+  
+  function isAuthenticated(req, res, next) {
+    if (req.session.userId) {
+      return next();
+    }
+    res.redirect('/login');
+  }
+app.post('/signin', async (req, res) => {
+    const { name, mobile, email,password } = req.body;
+  
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    try {
+      await pool.query('INSERT INTO "user" (user_name,email,mobile, password) VALUES ($1, $2,$3,$4)', [name, email, mobile, hashedPassword]);
+      res.redirect('/login'); // Redirect to login page after successful registration
+    } catch (error) {
+      console.error('Error during registration:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
 
 app.post("/home",(req,res)=>{
     let username = "Jeyabalan";
